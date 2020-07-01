ce4eca9e3c54c894007daab89247e1925b59bc8d
Keith Dumais <Keith Dumais@DESKTOP-ESL00L4>
2020-07-01 09:20:46 -0400

    setting up a new aem test

--- aem/src/test/java/aem/tests/AemOne.java
+++ aem/src/test/java/aem/tests/AemOne.java
@@ -1,11 +1,14 @@
 package aem.tests;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
 import aem.pages.AssetPage;
 import aem.pages.LogInPage;
 import aem.utils.Utils;
@@ -13,13 +16,18 @@ import aem.utils.Utils;
 public class AemOne extends BaseTest {
 	public WebDriver driver;
 	public Utils ut;
+	public static SoftAssert softAssert;
 	
 	@Parameters ({"ffProfileName", "browser"})
 	@BeforeMethod
 	public void beforeMethod(String ffProfileName, String browser, ITestResult result) {
 		driver = getDriver(ffProfileName, browser);
 		ut = new Utils(driver, prop);
+		Logger log = Logger.getLogger("Log");
 		result.setAttribute("utils", ut);
+		result.setAttribute("browser", browser);
+		result.setAttribute("logger", log);
+		softAssert = new SoftAssert();
 	}
 	
 	@AfterMethod
